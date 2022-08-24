1245a77659171b2b26919800f4f8eefdb3165f61
LizzieDworsky <amydworsky15@gmail.com>
2022-08-24 10:19:05 -0600

    working with python sets

--- practice.py
+++ practice.py
@@ -9,3 +9,22 @@ for power in my_superpowers:
 
 has_flight = "flight" in my_superpowers     #returns boolean value
 print(has_flight)
+
+some_other_powers = {"intellect", "super-speed"}
+my_superpowers.add("laser eyes")
+print(my_superpowers)
+my_superpowers.update(some_other_powers)
+print(my_superpowers)
+
+even_more_powers = ["shrinking", "teleportation"]
+my_superpowers.update(even_more_powers)
+print(my_superpowers)
+
+my_superpowers.remove("flight")
+print(my_superpowers)
+
+my_superpowers.discard("invisibility")
+
+powers_count = len(my_superpowers)
+
+print(f"There are {powers_count} powers in this set.")
