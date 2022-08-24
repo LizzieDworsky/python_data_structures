d5d54fe654e17eda066c728e39848300bcf45c92
LizzieDworsky <amydworsky15@gmail.com>
2022-08-24 13:20:36 -0600

    working on basics with Sets, function and math operations

--- practice_two.py
+++ practice_two.py
@@ -17,10 +17,39 @@ print(my_foods)
 my_foods.remove("burger")   #throws error if item doesn't exist
 my_foods.discard("shake")   #no error if item doesn't exist
 print(my_foods)
-# get a random value from the set
-some_food = my_foods.pop()
-print(my_foods)
-print(some_food)
-# .clear
-my_foods.clear()
-print(my_foods)
+            # get a random value from the set
+# some_food = my_foods.pop()
+# print(my_foods)
+# print(some_food)
+#             # .clear
+# my_foods.clear()
+# print(my_foods)
+
+# Write a function with a Set
+
+def food_checker(foods, food_to_find):
+    if food_to_find in foods:
+        print(f"{food_to_find} is here")
+    else:
+        print(f"{food_to_find} is not in this Set")
+
+food_checker(my_foods, "pizza")
+food_checker(my_foods, "hummus")
+
+# Math Operations
+david_foods = {"pizza", "lentil soup", "mac & cheese", "crab rangoon", "spinach mushroom lasagna"}
+megan_foods = {"watermelon", "mac & cheese", "frosted animal crackers", "pad thai"}
+amy_foods = {"pizza", "pad thai", "spaghetti squash", "tri-tip", "brownies", "mac & cheese"}
+# union
+my_union = david_foods.union(megan_foods, amy_foods)
+print(my_union)
+# intersection
+my_intersection = david_foods.intersection(megan_foods, amy_foods)
+print(my_intersection)
+# isdisjoint
+is_disjoint = david_foods.isdisjoint(megan_foods)
+print(is_disjoint)
+print(david_foods.isdisjoint(amy_foods))
+# difference
+my_difference = david_foods.difference(megan_foods, amy_foods)
+print(my_difference)
